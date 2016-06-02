1625942c70fbfdd010c1d898e5c79dd00d07ab3d
Vladislav Supalov <vladislav@supalov.com>
2016-06-02 22:04:50 +0200

    Initial commit.

---
+++ luigi/purchases.py
@@ -0,0 +1,104 @@
+import luigi
+import luigi.postgres
+import csv
+import datetime
+import psycopg2
+
+class ExternalPurchases(luigi.ExternalTask):
+    task_namespace = 'testing'
+
+    def output(self):
+        return luigi.LocalTarget("/home/ec2-user/code/data/purchases.csv")
+
+class PurchasesTotal(luigi.Task):
+    task_namespace = 'testing'
+
+    def output(self):
+        return luigi.LocalTarget("/home/ec2-user/code/data/luigi/total.txt")
+
+    def run(self):
+        total = 0
+        with self.input().open("r") as fin:
+            fin.next() #skip the header
+            total = sum(int(r[2])*int(r[3]) for r in csv.reader(fin))
+
+        with self.output().open("w") as out_file:
+            out_file.write(str(total))
+
+    def requires(self):
+       return ExternalPurchases()
+
+class PurchasesToDatabase(luigi.postgres.CopyToTable):
+    task_namespace = 'testing'
+
+    host = "localhost"
+    database = "dahoamdb"
+    user = "ec2-user"
+    password = "ec2-user"
+    table = "purchases"
+
+    columns = [("name", "TEXT"),
+               ("date", "TIMESTAMP"), #DATE would truncate at the day resolution
+               ("price", "INT"),
+               ("amount", "INT")]
+
+    def rows(self):
+        with self.input().open('r') as fobj:
+            fobj.next() #skip the header
+
+            for line in fobj:
+                split = line.strip('\n').split(', ')
+
+                #unix timestamp to date
+                date = datetime.datetime.fromtimestamp(
+                    int(split[1])
+                ).strftime('%Y-%m-%d %H:%M:%S')
+
+                yield (split[0], date, split[2], split[3])
+
+    def requires(self):
+       return ExternalPurchases()
+
+class QueryPostgres(luigi.Task):
+    task_namespace = 'testing'
+
+    def output(self):
+        return luigi.LocalTarget("/home/ec2-user/code/data/luigi/same_purchases.csv")
+
+    def requires(self):
+       return PurchasesToDatabase()
+
+    def run(self):
+        # these are here for convenience
+        host = "localhost"
+        database = "dahoamdb"
+        user = "ec2-user"
+        password = "ec2-user"
+
+        conn = psycopg2.connect(
+            dbname=database,
+            user=user,
+            host=host,
+            password=password)
+        cur = conn.cursor()
+        cur.execute("""SELECT
+          name,
+          date,
+          price,
+          amount
+          FROM purchases
+        """)
+        rows = cur.fetchall()
+
+        with self.output().open("w") as out_file:
+            out_file.write("name, date, price, amount") #write a header by hand
+            for row in rows:
+                out_file.write("\n")
+                # without the :%s, the date will be output in year-month-day format
+                out_file.write("{}, {:%s}, {}, {}".format(*row)) #each element of row by itself, unpacked by the star
+
+if __name__ == '__main__':
+    # if you want to execute one task per default without further arguments
+    #luigi.run(['testing.PurchasesToDatabase'])
+    luigi.run()
+
---
+++ run_luigi.sh
@@ -0,0 +1,4 @@
+python luigi/hello_world.py
+#python luigi/purchases.py testing.PurchasesTotal
+#python luigi/purchases.py testing.PurchasesToDatabase
+#python luigi/purchases.py testing.QueryPostgres
